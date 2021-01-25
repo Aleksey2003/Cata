4e55402ff05bb9c6252dcd512b3e571ffb2c0be5
Aleksey2003 <30956224+Aleksey2003@users.noreply.github.com>
2021-01-25 14:52:13 +0200

    table

--- method.rb
+++ method.rb
@@ -4,6 +4,5 @@ require 'dotenv/load'
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB_DB09_LGN'], password: ENV['DB_DB09_PWD'])
 
 client.query("use applicant_tests")
-results = client.query("SELECT FirstName, MiddleName, LastName FROM teachers_aleksey").to_a
-
+results = client.query("SELECT FirstName, MiddleName, LastName, BirthDate from teachers_aleksey").to_a
 p results
