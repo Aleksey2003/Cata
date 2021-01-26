b045d0a9337576651c20cb6f74d2b4128f03725e
Aleksey2003 <30956224+Aleksey2003@users.noreply.github.com>
2021-01-26 23:02:31 +0200

    project

--- methods.rb
+++ methods.rb
@@ -1,12 +1,15 @@
 def add_subject(name, client)
-  escaped = client.escape("('#{name}')")
+  escaped = client.escape(name)
   q = "insert into subjects_aleksey(Name) VALUES ('#{escaped}')"
-  p q
   client.query(q)
 end
 
 def finds(id, client)
-  f = "select FirstName, MiddleName, LastName, DAYOFMONTH(BirthDate) Day, MONTHNAME(BirthDate) Month, DAYNAME(BirthDate) Week from teachers_aleksey where ID = ('#{id}')"
-  p f
-  client.query(f)
+  f = "select FirstName, MiddleName, LastName, BirthDate from teachers_aleksey where ID = ('#{id}')"
+  results = client.query(f).to_a
+  if results.count == 0
+    puts "Teacher with id #{id} was not found"
+  else
+    puts "Teacher #{results[0]['FirstName']}" + "#{results[1]['MiddleName']}" + "#{results[2]['LastName']}" + "Born #{results[3]['BirthDate']}"
+  end
 end
