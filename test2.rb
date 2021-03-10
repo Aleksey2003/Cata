7a40de46e7fb838ea6e8b22a3a42d7c4495f4469
Aleksey2003 <30956224+Aleksey2003@users.noreply.github.com>
2021-03-10 22:20:53 +0200

    Project

--- test2.rb
+++ test2.rb
@@ -18,7 +18,7 @@ def candidates(client)
   a = "insert ignore into hle_dev_test_aleksey (candidate_office_name) select candidate_office_name from hle_dev_test_candidates"
   client.query(a)
 
-  tb = "select id, candidate_office_name from hle_dev_test_aleksey where clean_name and sentence is null"
+  tb = "select id, candidate_office_name from hle_dev_test_aleksey where clean_name is null"
   results = client.query(tb).to_a
 
   if results.count == 0
@@ -27,10 +27,10 @@ def candidates(client)
     results.each do |r|
       id = r['id']
       res = r['candidate_office_name']
-      res = res.gsub('Twp', 'Township').gsub(/Hwy|Highway highway|Hwy hwy/, 'Highway').gsub('Coroner', 'coroner').gsub('Clerk', 'clerk').gsub(/(.*)\/(.*)/){"#{Regexp.last_match[2].lstrip} #{Regexp.last_match[1].downcase}"}.gsub(/(.*)\/(.*)\/(.*)/){"#{Regexp.last_match[3]} #{Regexp.last_match[1].downcase.delete(' county ')} and #{Regexp.last_match[2].downcase}"}.gsub(/(.*),(.*)/){"#{Regexp.last_match[1]} (#{Regexp.last_match[2].lstrip})"}.gsub('Township township', 'Township').gsub('highway', 'Highway').gsub('County county', 'County')
+      res = res.gsub(/(?<![,\\\/\p{L}])\p{L}+/){|d| d.downcase}.gsub(/(.*)\/(.*)/){"#{Regexp.last_match[2].lstrip} #{Regexp.last_match[1].downcase}"}.gsub(/(.*)\/(.*)\/(.*)/){"#{Regexp.last_match[3]} #{Regexp.last_match[1].downcase.delete(' county ')} and #{Regexp.last_match[2].downcase}"}.gsub(/(.*),(.*)/){"#{Regexp.last_match[1]} (#{Regexp.last_match[2].lstrip.split.map(&:capitalize).join(" ")})"}.gsub(/County county|county county|County County/, 'County').gsub("clerk/recorder", 'clerk and recorder').gsub(/[.]/, '').gsub(/Park park|park park/, 'park').gsub('/', '').gsub(/Twp|twp|Township township|township/, 'Township').gsub(/Hwy|hwy|Highway highway|hwy hwy|Highway Highway|highway/, 'Highway').gsub('Township Township', 'Township').gsub('53 53', '53').gsub('District186', 'District 186').gsub('District153', 'District 153').gsub('village village', 'village').lstrip
 
       escaped = client.escape(res)
-      update = "UPDATE hle_dev_test_aleksey SET order by clean_name = '#{escaped}' desc WHERE ID = #{id}"
+      update = "UPDATE hle_dev_test_aleksey SET clean_name = '#{escaped}' WHERE ID = #{id}"
       client.query(update)
       p update
 
